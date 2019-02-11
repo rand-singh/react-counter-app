ca02ef9f8f3842aecf7853eabdaad2f37aa47488
Randeep Singh <randeeprattan90@gmail.com>
2019-02-11 20:22:27 +0000

    Updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
-  handleIncrement = product => {
+  handleIncrement = () => {
     this.setState({ value: this.state.value + 1 });
   };
 
   render() {
-    console.log("props", this.props);
-
     return (
       <div>
-        <h4>Counter #{this.props.id}</h4>
         <span className={this.getBadgeCLasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
